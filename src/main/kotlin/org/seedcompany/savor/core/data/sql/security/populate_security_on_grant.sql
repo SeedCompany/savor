91f5d0f8e9d1831b7c25f74af10a4c7f98ec66fa
questreal <vivekvarma15516@gmail.com>
2021-06-08 23:44:46 +0530

    add comments to the security function

--- src/main/kotlin/org/seedcompany/savor/core/data/sql/security/populate_security_on_grant.sql
+++ src/main/kotlin/org/seedcompany/savor/core/data/sql/security/populate_security_on_grant.sql
@@ -191,22 +191,23 @@ begin
 	p_column_name := '_' || new.column_name;
 	p_access_level := new.access_level;
 	raise info '%', p_table_name;
+	-- checking if there are members for the role
 	select count(*) from sys_role_memberships
 	into role_membership_count
 	where sys_role_id = new.sys_role_id;
 	
 	if role_membership_count > 0 then
-	
+	-- looping over each person belonging to the role
 		for rec1 in (select sys_person_id from sys_role_memberships
 				     where sys_role_id = new.sys_role_id) loop
-					 
--- 			select count(*) from quote_ident(p_table_name) into entries_count_for_person
--- 			where __sys_person_id = rec1.sys_person_id;
 
+	-- checking if the person has entries in the security table
 		execute format('select count(*) from ' || quote_ident(p_table_name) ||' where __sys_person_id = '||rec1.sys_person_id)
 		into entries_count_for_person;
-			
+	
 			if entries_count_for_person = 0 then
+			-- loop over each record in the base table and insert the person id into security table 
+			-- update the access level from NULL to Read/Write for the specific column
 				for rec2 in execute format('select * from ' || p_base_table_name) loop 
 				
 					execute format('insert into '|| p_table_name || '(__sys_person_id) values(' || 
@@ -215,10 +216,10 @@ begin
 								  || ' = ' || quote_literal(p_access_level)|| ' where __sys_person_id = '|| rec1.sys_person_id);
 			    end loop;
 			else 
--- 				select p_column_name from p_table_name into current_access_level 
--- 				where __sys_person_id = rec1.sys_person_id limit 1;
+			--  get the current access level of the column
 				execute format('select '|| p_column_name || ' from ' || p_table_name || ' 
 				where __sys_person_id =' || rec1.sys_person_id || ' limit 1') into current_access_level;
+			--  if the access level isn't write, then update it 
 				if current_access_level != 'Write' then
 					execute format('update '|| p_table_name || ' set ' || p_column_name 
 								  || ' = ' || quote_literal(p_access_level)|| ' where __sys_person_id = '|| rec1.sys_person_id);
@@ -231,8 +232,10 @@ begin
 end; $$;
 
 
+DROP TRIGGER sys_role_grants_insert_trigger on sys_role_grants;
+
 CREATE TRIGGER sys_role_grants_insert_trigger
-  AFTER INSERT
-  ON sys_role_grants
-  FOR EACH ROW
-  EXECUTE PROCEDURE populate_security_on_grant_fn();
+AFTER INSERT
+ON sys_role_grants
+FOR EACH ROW
+EXECUTE PROCEDURE populate_security_on_grant_fn();
