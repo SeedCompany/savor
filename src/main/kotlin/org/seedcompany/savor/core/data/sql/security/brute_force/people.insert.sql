70c531f7f629d3933c9042626392a04fa5c42c47
questreal <vivekvarma15516@gmail.com>
2021-06-18 01:21:07 +0530

    reorganize security functions

---
+++ src/main/kotlin/org/seedcompany/savor/core/data/sql/security/brute_force/people.insert.sql
@@ -0,0 +1,25 @@
+-- new.id
+create or replace function insert_person_to_security(p_schema_name text)
+returns void
+language plpgsql
+as $$
+declare 
+	rec1 record;
+    rec2 record;
+    security_table_name text;
+begin
+    execute format('set schema '|| quote_literal(p_schema_name));
+
+	for rec1 in (select table_name from information_schema.tables where table_schema = p_schema_name and table_name like '%_data' order by table_name) loop 
+
+        raise info 'table_name: %', table_name;
+
+        for rec2 in execute format('select id from '|| rec1.table_name) loop 
+
+            security_table_name := rec1.table_name || '_security';
+            execute format('insert into '|| security_table_name || '(__id, __person_id) values (' || rec2.id || ',' || new.id || ')' );
+
+        end loop;
+    end loop;
+    raise info 'done';
+end; $$
