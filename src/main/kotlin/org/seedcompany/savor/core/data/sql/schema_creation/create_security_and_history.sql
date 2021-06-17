8d7130ca2350c6d9042b93381b1a1a5ecd24880a
questreal <vivekvarma15516@gmail.com>
2021-06-17 21:56:55 +0530

    make schema creation function idempotent

--- src/main/kotlin/org/seedcompany/savor/core/data/sql/schema_creation/create_security_and_history.sql
+++ src/main/kotlin/org/seedcompany/savor/core/data/sql/schema_creation/create_security_and_history.sql
@@ -1,16 +1,20 @@
+-- NOTE: using pg_catalog instead of information_schema might speed up the function
 create or replace function create_security_history_tables(p_schema_name text)
-returns void
+returns text
 language plpgsql
 as $$
 declare 
 	rec1 record;
     rec2 record;
+    existing_column text;
     p_table_name_literal text;
     p_table_name text; 
     history_table_name text;
     security_table_name text;
 	security_table_column text;
+	status text;
 begin
+	status := 'no change';
     -- FINDING ALL TABLES THAT NEED A HISTORY AND SECURITY TABLE AND LOOPING OVER THEM
     for rec1 in (select table_name
 	from information_schema.tables
@@ -25,35 +29,53 @@ begin
         -- "locations_security"
         history_table_name := quote_ident(p_table_name_literal||'_history');
         -- "locations_history"
-
-        -- DROP HISTORY AND SECURITY TABLES IF THEY EXIST
-        execute format('drop table if exists '|| security_table_name || ' cascade ');
-        execute format('drop table if exists '|| history_table_name || ' cascade ');
-
         p_table_name := quote_ident(rec1.table_name);
         -- "locations_data"
+
 		raise info 'table_name: %', p_table_name;
         -- HISTORY TABLE CREATION
         execute format('create table if not exists '|| history_table_name || ' ( _history_id serial primary key, 
         _history_created_at timestamp not null default CURRENT_TIMESTAMP)'); 
 
         -- SECURITY TABLE CREATION
-        execute format('create table if not exists '|| security_table_name || ' ( __person_id int not null, __id int not null, foreign key (__id) references ' ||  p_table_name || '(id))' );
+        execute format('create table if not exists '|| security_table_name || ' ( __person_id int not null, __id int not null, foreign key(__person_id) references public.people_data(id), foreign key (__id) references ' ||  p_table_name || '(id))' );
 
 
         -- UPDATE BOTH SECURITY AND HISTORY TABLE 
          for rec2 in (select column_name,data_type from information_schema.columns
         where table_schema = p_schema_name and table_name = p_table_name) loop
 		raise info 'col-name: % | data-type: %', rec2.column_name, rec2.data_type;
-            execute format('alter table ' || history_table_name || ' add column ' || rec2.column_name || ' ' ||
-            rec2.data_type);
+
+            select column_name from information_schema.columns into existing_column where table_schema = p_schema_name
+            and table_name = history_table_name and column_name = rec2.column_name ;
+
+            if not found then
+				
+				status := 'history table updated';
+                execute format('alter table ' || history_table_name || ' add column ' || rec2.column_name || ' ' ||
+                rec2.data_type);
+
+            end if;
+
+
 			security_table_column := '_' || rec2.column_name;
-            execute format('alter table '|| security_table_name || ' add column '|| security_table_column || ' public.access_level');
+
+            select column_name from information_schema.columns into existing_column where table_schema = p_schema_name and table_name = security_table_name and column_name = security_table_column ;
+
+            if not found then 
+				
+				status := 'security table updated';
+                execute format('alter table '|| security_table_name || ' add column '|| security_table_column || ' public.access_level');
+            
+            end if;
+
         end loop;
 
 	end loop;
 	raise info 'DONE';
+	return status;
 end; $$
+
 
 select create_security_history_tables('public');
 select create_security_history_tables('sc');
