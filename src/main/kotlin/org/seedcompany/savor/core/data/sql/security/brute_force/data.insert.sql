c7ae3e838d645cacac9253b6b2a3ac6d51f55632
Vivek Varma <vivekvarma15516@gmail.com>
2021-06-20 11:35:19 +0530

    people.insert and data.insert functions tested

--- src/main/kotlin/org/seedcompany/savor/core/data/sql/security/brute_force/data.insert.sql
+++ src/main/kotlin/org/seedcompany/savor/core/data/sql/security/brute_force/data.insert.sql
@@ -1,18 +1,23 @@
 -- inserts the new id into the security table for each member 
+-- trigger function for each data table
 create or replace function insert_data_to_security()
-returns void
+returns trigger
 language plpgsql
 as $$
 declare 
 security_schema_table text;
 rec1 record;  
 begin
-        security_schema_table := quote_ident(TG_TABLE_SCHEMA || '.' || TG_TABLE_NAME);
-        -- public.locations_security
+        security_schema_table := TG_TABLE_SCHEMA || '.' || TG_TABLE_NAME;
+		security_schema_table := replace(security_schema_table, '_data', '_security');
+		raise info 'security table: %', security_schema_table;
+		
         
-        for rec1 in execute format('select __person_id from '|| security_schema_table) loop
+         for rec1 in execute format('select id from public.people_data') loop
 
-            execute format('insert into '|| security_schema_table || '(__id, __person_id) values (' || new.id || ',' || rec1.person_id ')'); 
+             execute format('insert into '|| security_schema_table || '(__id, __person_id) values (' || new.id || ',' || quote_literal(rec1.id) || ')'); 
             
-        end loop; 
+         end loop; 
+		return new;
 end; $$
+
--- src/main/kotlin/org/seedcompany/savor/core/data/sql/security/brute_force/people.insert.sql
+++ src/main/kotlin/org/seedcompany/savor/core/data/sql/security/brute_force/people.insert.sql
@@ -1,6 +1,6 @@
 -- trigger function on insert for public.people
-create or replace function insert_person_to_security(p_schema_name text)
-returns void
+create or replace function insert_person_to_security()
+returns trigger
 language plpgsql
 as $$
 declare 
@@ -8,18 +8,36 @@ declare
     rec2 record;
     security_table_name text;
 begin
-    execute format('set schema '|| quote_literal(p_schema_name));
+    execute format('set schema '|| quote_literal(TG_ARGV[0]));
 
-	for rec1 in (select table_name from information_schema.tables where table_schema = p_schema_name and table_name like '%_data' order by table_name) loop 
+	for rec1 in (select table_name from information_schema.tables where table_schema = TG_ARGV[0] and table_name like '%_data' order by table_name) loop 
 
-        raise info 'table_name: %', table_name;
+        raise info 'table_name: %', rec1.table_name;
 
         for rec2 in execute format('select id from '|| rec1.table_name) loop 
 
-            security_table_name := rec1.table_name || '_security';
+            security_table_name := replace(rec1.table_name, '_data', '_security');
+			raise info 'security_table_name: %', security_table_name;
             execute format('insert into '|| security_table_name || '(__id, __person_id) values (' || rec2.id || ',' || new.id || ')' );
 
         end loop;
     end loop;
     raise info 'done';
+	return new;
 end; $$
+
+drop trigger if exists insert_people_public_security_trigger on public.people_data;
+drop trigger if exists insert_people_sc_security_trigger on public.people_data;
+
+
+create trigger insert_people_public_security_trigger 
+after insert 
+on public.people_data
+for each row 
+execute procedure public.insert_person_to_security('public');
+
+create trigger insert_people_sc_security_trigger 
+after insert 
+on public.people_data
+for each row 
+execute procedure public.insert_person_to_security('sc');
