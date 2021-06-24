7580b08896db3a2f65aca6cf880573f282a15634
Vivek Varma <vivekvarma15516@gmail.com>
2021-06-24 18:31:52 +0530

    modify schema_creation function to be more readable

--- src/main/kotlin/org/seedcompany/savor/core/data/sql/schema.creation.sql
+++ src/main/kotlin/org/seedcompany/savor/core/data/sql/schema.creation.sql
@@ -3,7 +3,7 @@
 -- NOTE: if function needs to be extended for multi-dimensional array datatypes for columns - https://stackoverflow.com/questions/39436189/how-to-get-the-dimensionality-of-an-array-column
 
 
-create or replace function create_security_history_tables(p_schema_name text)
+create or replace function public.create_security_history_tables(p_schema_name text)
 returns text
 language plpgsql
 as $$
@@ -11,10 +11,11 @@ declare
 	rec1 record;
     rec2 record;
     existing_column text;
-    p_table_name_literal text;
-    p_table_name text; 
-    history_table_name text;
+    base_schema_table_name text;
     security_table_name text;
+    history_table_name text;
+    security_schema_table_name text;
+    history_schema_table_name text;    
 	security_table_column text;
 	status text;
 begin
@@ -25,34 +26,33 @@ begin
 	where table_schema = p_schema_name and table_name like '%_data'
 	order by table_name) loop 
 
-		execute format('set schema '|| quote_literal(p_schema_name));
-        
-        p_table_name_literal := replace(rec1.table_name, '_data', '');
-        -- locations_data -> locations
-        security_table_name := quote_ident(p_table_name_literal||'_security');
-        -- "locations_security"
-        history_table_name := quote_ident(p_table_name_literal||'_history');
-        -- "locations_history"
-        p_table_name := quote_ident(rec1.table_name);
-        -- "locations_data"
+        -- locations_security, locations_history, 
+        security_table_name := replace(rec1.table_name, '_data', '_security');
+        history_table_name := replace(rec1.table_name, '_data', '_history');
+        raise info 'history_table_name: % | security_table_name: %', history_table_name, security_table_name;
 
-		raise info 'table_name: %', p_table_name;
+        -- public.locations_security, public.locations_history
+        base_schema_table_name := p_schema_name || '.' || rec1.table_name;
+        security_schema_table_name := replace(base_schema_table_name, '_data', '_security');
+        history_schema_table_name := replace(base_schema_table_name, '_data', '_history');
+        raise info 'history_schema_table_name: % | security_schema_table_name: %', history_schema_table_name, security_schema_table_name;
+
         -- HISTORY TABLE CREATION
-        execute format('create table if not exists '|| history_table_name || ' ( _history_id serial primary key, 
+        execute format('create table if not exists '|| history_schema_table_name || ' ( _history_id serial primary key, 
         _history_created_at timestamp not null default CURRENT_TIMESTAMP)'); 
 
         -- SECURITY TABLE CREATION
-        execute format('create table if not exists '|| security_table_name || ' ( __person_id int not null, __id int not null, __is_cleared boolean, foreign key(__person_id) references public.people_data(id), foreign key (__id) references ' ||  p_table_name || '(id))' );
+        execute format('create table if not exists '|| security_schema_table_name || ' ( __person_id int not null, __id int not null, __is_cleared boolean, foreign key(__person_id) references public.people_data(id), foreign key (__id) references ' ||  base_schema_table_name || '(id))' );
 
 
-        -- UPDATE BOTH SECURITY AND HISTORY TABLE 
+        -- UPDATE BOTH SECURITY AND HISTORY TABLE (IDEMPOTENT MANNER)
          for rec2 in (select column_name,case 
         			  when (data_type = 'USER-DEFINED') then udt_name
                       when (data_type = 'ARRAY')
                       then substr(udt_name, 2, length(udt_name)-1) || '[]' 
         			else data_type 
     				end as data_type from information_schema.columns
-        			where table_schema = p_schema_name and table_name = p_table_name) loop
+        			where table_schema = p_schema_name and table_name = rec1.table_name) loop
 		raise info 'col-name: % | data-type: %', rec2.column_name, rec2.data_type;
 
             select column_name from information_schema.columns into existing_column where table_schema = p_schema_name
@@ -61,7 +61,7 @@ begin
             if not found then
 				
 				status := 'history table updated';
-                execute format('alter table ' || history_table_name || ' add column ' || rec2.column_name || ' ' ||
+                execute format('alter table ' || history_schema_table_name || ' add column ' || rec2.column_name || ' ' ||
                 rec2.data_type);
 
             end if;
@@ -74,7 +74,7 @@ begin
             if not found then 
 				
 				status := 'security table updated';
-                execute format('alter table '|| security_table_name || ' add column '|| security_table_column || ' public.access_level');
+                execute format('alter table '|| security_schema_table_name || ' add column '|| security_table_column || ' public.access_level');
             
             end if;
 
@@ -86,5 +86,5 @@ begin
 end; $$;
 
 
-select create_security_history_tables('public');
-select create_security_history_tables('sc');
+select public.create_security_history_tables('public');
+select public.create_security_history_tables('sc');
