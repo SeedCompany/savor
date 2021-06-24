a88e3026d8a6cf99c0b13c9bdea6fde25c75b649
Vivek Varma <vivekvarma15516@gmail.com>
2021-06-24 20:42:46 +0530

    project_memberships_data trigger for sensitivity

---
+++ src/main/kotlin/org/seedcompany/savor/core/data/sql/security/brute_force/project.memberships.sensitivity.sql
@@ -0,0 +1,22 @@
+-- go to locations_security and update all the __is_cleared columns 
+
+create or replace function refresh__location_is_cleared_on_project_memberships()
+returns trigger 
+language plpgsql
+as $$
+declare
+rec1 record; 
+project_location_id int;
+begin 
+    -- new.project, new.person
+    select primary_location into project_location_id from public.projects_data where id = new.project; 
+    update public.locations_security set __is_cleared = true where __person_id = new.person and __id = project_location_id;
+return new;
+end; $$;
+
+drop trigger if exists location_project_memberships  on public.projects_data;
+
+create trigger location_project_memberships after insert 
+on public.project_memberships_data 
+for each row 
+execute procedure refresh__location_is_cleared_on_project_memberships();
