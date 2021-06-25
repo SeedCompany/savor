60fe7a16c303184046d6b711edf39f4c36e4c3f9
Vivek Varma <vivekvarma15516@gmail.com>
2021-06-25 19:08:46 +0530

    history trigger function refactored

--- src/main/kotlin/org/seedcompany/savor/core/data/sql/history/common_trigger_function.sql
+++ src/main/kotlin/org/seedcompany/savor/core/data/sql/history/common_trigger_function.sql
@@ -1,25 +1,26 @@
 CREATE OR REPLACE FUNCTION common_history_fn()
-  RETURNS TRIGGER
-  LANGUAGE PLPGSQL
-  AS $$
-  declare 
-  original_table_name text;
-  history_table_name text;
-  sql_statement text;
-  rec record;
-  last_id int;
-  col_name text;
+RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS $$
+declare 
+base_schema_table_name text;
+history_schema_table_name text;
+rec1 record;
+col_name text;
+last_id int;
 begin
-    original_table_name := quote_ident(TG_ARGV[0]);
-    history_table_name := original_table_name || '_history';
-    raise info '%', original_table_name; 
-	execute format('insert into '|| history_table_name || '(_history_id) values( default
+  base_schema_table_name := TG_TABLE_SCHEMA || '.' || TG_TABLE_NAME;
+  history_schema_table_name := replace(base_schema_table_name, '_data', '_history');
+
+  raise info '%', base_schema_table_name; 
+	execute format('insert into '|| history_schema_table_name || '(_history_id) values( default
 	 ) returning _history_id') into last_id;
-	for rec in (select column_name from information_schema.columns where table_name = original_table_name) loop 
-	col_name := quote_ident(rec.column_name);
-	raise info '%', rec;
-	raise info '%', col_name;
-    execute format('update '|| history_table_name || ' set ' || col_name || ' = $1.' || col_name ||' where _history_id = '|| last_id) using new;
+
+	for rec1 in (select column_name from information_schema.columns where table_schema = TG_TABLE_SCHEMA and  table_name = TG_TABLE_NAME) loop 
+    raise info '%', rec1;
+    raise info '%', rec1.column_name;
+    col_name := quote_ident(rec1.column_name);
+      execute format('update '|| history_schema_table_name || ' set ' || quote_ident(rec1.column_name) || ' = $1.' || col_name ||' where _history_id = '|| last_id) using new ;
 	END loop;
 	raise info 'DONE';
 	RETURN NEW;
--- src/main/kotlin/org/seedcompany/savor/core/data/sql/history/create_all_triggers.sql
+++ src/main/kotlin/org/seedcompany/savor/core/data/sql/history/create_all_triggers.sql
@@ -1,41 +1,41 @@
-CREATE OR REPLACE FUNCTION create_history_triggers_fn()
+CREATE OR REPLACE FUNCTION create_history_triggers_fn(p_schema_name text)
 RETURNS VOID
 LANGUAGE PLPGSQL
 AS $$
 declare 
-	 rec record;
-  	 p_table_name text;
-	 p_table_name_literal text;
+	 rec1 record;
 	 insert_trigger_name text;
 	 update_trigger_name text;
+	 base_schema_table_name text;
 begin
-	for rec in (SELECT table_name
+	for rec1 in (SELECT table_name
 	FROM information_schema.tables
-	WHERE table_schema = 'public' and table_name like '%history'
+	WHERE table_schema = p_schema_name and table_name like '%_data'
 	ORDER BY table_name) loop 
 	-- FINDING ALL TABLES THAT HAVE A HISTORY TABLE AND LOOPING OVER THEM
-	p_table_name_literal := replace(rec.table_name, '_history', '');
-	p_table_name := quote_ident(p_table_name_literal);
-	insert_trigger_name := quote_ident(p_table_name_literal||'_history_insert_trigger');
-	update_trigger_name := quote_ident(p_table_name_literal||'_history_update_trigger');
-	raise info '%', p_table_name;
+	base_schema_table_name = p_schema_name || '.' || rec1.table_name;
+	insert_trigger_name := quote_ident(rec1.table_name||'_history_insert_trigger');
+	update_trigger_name := quote_ident(rec1.table_name||'_history_update_trigger');
+	raise info '%', base_schema_table_name;
 	-- INSERT TRIGGER
-	execute format('DROP TRIGGER IF EXISTS '|| insert_trigger_name || ' ON ' ||p_table_name);
+	execute format('DROP TRIGGER IF EXISTS '|| insert_trigger_name || ' ON ' ||base_schema_table_name);
 	execute format('CREATE TRIGGER ' || insert_trigger_name
   	|| ' AFTER INSERT
-  	ON ' || p_table_name || 
+  	ON ' || base_schema_table_name || 
   	' FOR EACH ROW
-  	EXECUTE PROCEDURE common_history_fn('|| p_table_name_literal || ')'); 
+  	EXECUTE PROCEDURE common_history_fn()'); 
 	--   UPDATE TRIGGER
-	execute format('DROP TRIGGER IF EXISTS ' || update_trigger_name || ' ON ' || p_table_name);
+	execute format('DROP TRIGGER IF EXISTS ' || update_trigger_name || ' ON ' || base_schema_table_name);
 	execute format('CREATE TRIGGER ' || update_trigger_name
   	|| ' AFTER UPDATE
-  	ON ' || p_table_name || 
+  	ON ' || base_schema_table_name || 
   	' FOR EACH ROW
-  	EXECUTE PROCEDURE common_history_fn('|| p_table_name_literal || ')'); 
+  	EXECUTE PROCEDURE common_history_fn()'); 
 
 	END loop;
 	raise info 'DONE';
 end; $$;
 
-select create_all_triggers_fn();
+select create_history_triggers_fn('public');
+select create_history_triggers_fn('sc');
+select create_history_triggers_fn('sil');
