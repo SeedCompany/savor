8b6a1f6a027562cade9bae3276826f568e59135c
Vivek Varma <vivekvarma15516@gmail.com>
2021-06-27 17:57:45 +0530

    people_data table update trigger fn for sensitivity

--- src/main/kotlin/org/seedcompany/savor/core/data/sql/security/sensitivity/new/people.sensitivity.sql
+++ src/main/kotlin/org/seedcompany/savor/core/data/sql/security/sensitivity/new/people.sensitivity.sql
@@ -1,6 +1,4 @@
 -- go to every security table and update the __is_cleared column for every id 
-
--- trigger function on insert for public.people
 create or replace function public.people_sensitivitiy()
 returns trigger
 language plpgsql
@@ -10,7 +8,7 @@ declare
     rec2 record;
     base_schema_table_name text;
     security_schema_table_name text;
-    row_sensitivity_clearance boolean;
+    data_table_row_sensitivity public.sensitivity;
 begin
     -- execute format('set schema '|| quote_literal(TG_ARGV[0]));
 
@@ -19,35 +17,44 @@ begin
         raise info 'table_name: %', rec1.table_name;
         base_schema_table_name := TG_ARGV[0] || '.' || rec1.table_name;
 
+    perform column_name 
+    FROM information_schema.columns 
+    WHERE table_schema = TG_ARGV[0] and table_name = rec1.table_name and column_name='sensitivity';
 
-            for rec2 in execute format('select id from '|| base_schema_table_name) loop 
+    if found then 
+        base_schema_table_name := TG_ARGV[0] || '.' || rec1.table_name;
+        security_schema_table_name := replace(base_schema_table_name, '_data', '_security');
 
-                raise info 'people.insert.fn rec2: %', rec2;
+        execute format('select sensitivity from ' || base_schema_table_name || ' where id = ' || p_id) into data_table_row_sensitivity;
 
-                select public.get_sensitivity_clearance(rec2.id, new.id, new.sensitivity_clearance, TG_ARGV[0], rec1.table_name) into row_sensitivity_clearance;
-                security_schema_table_name := replace(base_schema_table_name, '_data', '_security');
-                raise info 'security_schema_table_name: %', security_schema_table_name;
-                execute format('insert into '|| security_schema_table_name || '(__id, __person_id, __is_cleared) values (' || rec2.id || ',' || new.id || ',' || row_sensitivity_clearance || ')' );
+        raise info 'data_table_row_sensitivity: % | person_sensitivity_clearance: %', data_table_row_sensitivity, new.sensitivity_clearance;
+        
+        if (data_table_row_sensitivity = 'Medium' and new.sensitivity_clearance = 'Low') or 
+        (data_table_row_sensitivity = 'High' and (new.sensitivity_clearance = 'Medium' or new.sensitivity_clearance = 'Low')) then 
 
-            end loop;
+            execute format('update ' || security_schema_table_name || ' set __is_cleared = false where __person_id = '|| old.__person_id || ' and '|| ' __id = '|| new.__id);
+    
+        end if;    
+    end if;    
+
 
     end loop;
     raise info 'done';
 	return new;
 end; $$;
 
-drop trigger if exists insert_people_public_security_trigger on public.people_data;
-drop trigger if exists insert_people_sc_security_trigger on public.people_data;
+drop trigger if exists update_people_public_sensitivity_trigger on public.people_data;
+drop trigger if exists update_people_sc_sensitivity_trigger on public.people_data;
 
 
-create trigger insert_people_public_security_trigger 
-after insert 
+create trigger update_people_public_sensitivity_trigger 
+after update 
 on public.people_data
 for each row 
 execute procedure public.people_sensitivitiy('public');
 
-create trigger insert_people_sc_security_trigger 
-after insert 
+create trigger update_people_sc_sensitivity_trigger 
+after update 
 on public.people_data
 for each row 
 execute procedure public.people_sensitivitiy('sc');
